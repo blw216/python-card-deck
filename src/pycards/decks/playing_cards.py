4d80b4939c8e64b5ed55841b1f1b8498b6f49db2
Brady Woods <brady@blawoods@icloud.com>
2023-09-22 16:16:10 -0400

    added documentation

--- src/pycards/decks/playing_cards.py
+++ src/pycards/decks/playing_cards.py
@@ -4,22 +4,19 @@ from ..exceptions import ShuffleError, DealError
 
 class PlayingCards(BaseDeck):
     """
-    PlayingCards is an implementation of the BaseDeck
-    abstract class, consisting of the standard deck of
-    playing cards.
+    PlayingCards is an implementation of the BaseDeck abstract class,
+    consisting of the standard deck of 52 playing card across four suites.
     """
     def __init__(self):
         """
-        This class represents a standard 52-card playing deck. It is
-        an extension of the `BaseDeck` abstract base class, which
-        requires two methods, "shuffle", and "deal_one_card".
+        This constructor method represents a standard 52-card playing deck.
         """
         self.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
         self.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
         self.deck = [{'rank': rank, 'suit': suit} for rank in self.ranks for suit in self.suits]
         self.deck_length = len(self.deck)
 
-    def shuffle(self):
+    def shuffle(self) -> None:
         """
         The shuffle method performs the Fisher-Yates/Knuth
         shuffling algorithm, which randomly shuffles the
@@ -30,7 +27,7 @@ class PlayingCards(BaseDeck):
         This method has no parameters.
 
         :returns: None
-        :raises DeckError: raises a DeckError exception if the deck cannot
+        :raises ShuffleError: raises a ShuffleError exception if the deck cannot
         be shuffled.
         """
         # Get the length of the list
@@ -44,11 +41,27 @@ class PlayingCards(BaseDeck):
         else:
             raise ShuffleError(self.deck_length)
     
-    def deal_one_card(self):
+    def deal_one_card(self) -> dict:
+        """
+        The `deal_one_card` method returns the "top" (position 0) card
+        of the deck at the time the method is called. The dealt card
+        is subsequently removed from the deck when it is dealt.
+
+        This method has no parameters.
+
+        :returns: card
+        :rtype: [dict | None]
+        :raises DealError: This exception is raised if there are no cards
+        left to deal.
+        """
+        # If there are cards left to deal
         if len(self.deck) > 0:
-            print(self.deck_length)
+            # Extract the top card of the deck
             card = self.deck.pop(0)
+            # Recalculate the number of cards left in the deck
             self.deck_length = len(self.deck)
+            # Return the card to the user
             return card
+        # Else raise the DealError exception
         else:
             raise DealError(self.deck_length)
